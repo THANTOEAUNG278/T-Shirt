9e2490527c2f9b57cb68e1cd1a91e42113498daa
THANTOEAUNG <kyawshein034@gmail.com>
2024-05-23 15:52:54 +0900

    change

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,7 +1,43 @@
+import PhotoSlide from "./PhotoSlide";
+
+const clothes =[
+  {
+    id:1,
+    title:"T-Shirt",
+    imageUrl:"",
+    price:"￥3500"
+  },
+  {
+    id:2,
+    title:"T-Shirt",
+    imageUrl:"",
+    price:"￥3500"
+  },
+  {
+    id:3,
+    title:"T-Shirt",
+    imageUrl:"",
+    price:"￥3500"
+  },
+  {
+    id:4,
+    title:"T-Shirt",
+    imageUrl:"",
+    price:"￥3500"
+  },
+  {
+    id:5,
+    title:"T-Shirt",
+    imageUrl:"",
+    price:"￥3500"
+  },
+]
+
+
 const HomePages = () =>{
   return(
     <div>
-      hello Homepages
+      <PhotoSlide/>
     </div>
   )
 }
